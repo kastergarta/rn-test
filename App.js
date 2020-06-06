76e1461f8780a8990a55bd6773a40db5fc505e70
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-06-06 02:02:50 -0400

    next step

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoal, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText){
+    setEnteredGoal(enteredText);
+  }
+
+  function addGoalHandler(){
+    console.log(enteredGoal)
+  }
+
   return (
-    <View>
-      <View style ={{padding: 30}}>
+    <View style ={styles.screen}>
+      <View style ={styles.inputContainer}>
         <TextInput 
           placeholder='Course goal' 
-          style={{borderColor: 'blue', borderWidth: 1}}
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title='ADD'/>
+        <Button title='ADD' onPress={addGoalHandler}/>
+      </View>
+      <View>
+
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  input: {
+    borderColor: 'blue',
+    borderWidth: 1,
+    padding: 10
+  }
 });
