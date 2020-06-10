d428530fe3049966cd015cff4895b039641a91de
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-06-10 09:37:55 -0400

    remove item

--- App.js
+++ App.js
@@ -9,13 +9,23 @@ export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
 
   function addGoalHandler(goalTitle){
-    setCourseGoals(currentGoals => [...currentGoals, {key: Math.random().toString(), value: goalTitle}]);
+    setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: goalTitle}]);
+  }
+  
+  const removeGoalHandler = goalId => {
+    console.log(goalId)
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
   }
 
   return (
     <View style ={styles.screen}>
       <GoalInput onAddGoal={addGoalHandler}/>
-      <FlatList data={courseGoals} renderItem={itemData => <GoalItem title={itemData.item.value}/> } />
+      <FlatList 
+      keyExtractor={(item, index) => item.id}
+      data={courseGoals} 
+      renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value}/> } />
     </View>
   );
 }
